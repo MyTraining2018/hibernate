601f0ff51b35eba7aaf7192f1123725938ef4d7e
MyTraining2018 <captrainingnov2016@gmail.com>
2018-04-23 15:36:08 +0530

    Add files via upload

---
+++ HibernateUtil.java
@@ -0,0 +1,36 @@
+package org.cap.demo;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory;
+
+	//private static ServiceRegistry serviceRegistry;
+
+	static {
+		try {
+			
+			
+		StandardServiceRegistry standardRegistry = 
+		new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		Metadata metaData = 
+		new MetadataSources(standardRegistry).getMetadataBuilder().build();
+			sessionFactory = metaData.getSessionFactoryBuilder().build();
+		} catch (Throwable th) {
+
+			System.err.println("Initial SessionFactory creation failed" + th);
+			throw new ExceptionInInitializerError(th);
+
+		}
+	}
+	public static SessionFactory getSessionFactory() {
+
+		return sessionFactory;
+
+	}
+	
+}
